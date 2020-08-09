c6f3bebe4b39dd7209911ef4e925e3e71db44d13
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-09 15:38:50 +0300

    Made checkers color depends on constants

--- src/components/Checker.jsx
+++ src/components/Checker.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
 import 'styles/Checker.css'
-import { PLAYERS_TAGS } from 'js/constants';
+import { PLAYERS_TAGS, PLAYERS_COLORS } from 'js/constants';
 
 const Checker = ({checker, selectChecker}) => {
-  const color = (
-    (checker.player === PLAYERS_TAGS.PLAYER1) ? 'blue' : 'green'
-  );
+  const color = (() => {
+    if (checker.player === PLAYERS_TAGS.PLAYER1) {
+      return PLAYERS_COLORS.PLAYER1;
+    } else {
+      return PLAYERS_COLORS.PLAYER2;
+    }
+  })();
 
   const clickHandler = selectChecker.bind(null, checker);
   
   return (
     <div 
-      className={`Checker Checker_${color}`}
+      className={'Checker'}
+      style={{backgroundColor: color}}
       onClick={clickHandler}
     />
   );
