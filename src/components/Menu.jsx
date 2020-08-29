e0db4ffe04cde6c01e1311bd6c9eb922f47b1cd6
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-29 20:13:47 +0300

    Created Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import 'styles/Menu.css'
+import Button from './Button'
+
+const Menu = () => {
+  return (
+    <div class='Menu'>
+      <Button>Create room</Button>
+      <Button>Join</Button>
+    </div>
+  )
+};
+
+export default Menu;
