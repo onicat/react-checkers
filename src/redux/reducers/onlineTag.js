2fd71daebf5072cbed20954005e90743ebbd3c3b
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-30 03:05:33 +0300

    Created changeOnlineTag action handler in onlineTag reducer

--- src/redux/reducers/onlineTag.js
+++ src/redux/reducers/onlineTag.js
@@ -1,7 +1,15 @@
 import produce from 'immer'
+import actionTypes from 'redux/actionTypes';
+import { PLAYERS_TAGS } from 'js/constants';
 
 const onlineTag = produce((state, action) => {
-
+  switch(action.type) {
+    case actionTypes.CHANGE_ONLINE_TAG: {
+      return (action.playerTag === PLAYERS_TAGS.PLAYER1) ?
+        PLAYERS_TAGS.PLAYER1:
+        PLAYERS_TAGS.PLAYER2
+    }
+  }
 }, null);
 
 export default onlineTag;
