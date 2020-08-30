647f8f211480f4710c9c7ec2d65064b7f710c02d
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-30 03:00:59 +0300

    Renamed yourOnlineTag to onlineTag

---
+++ src/redux/reducers/onlineTag.js
@@ -0,0 +1,7 @@
+import produce from 'immer'
+
+const onlineTag = produce((state, action) => {
+
+}, null);
+
+export default onlineTag;
--- src/redux/reducers/yourOnlineTag.js
+++
@@ -1,7 +0,0 @@
-import produce from 'immer'
-
-const yourOnlineTag = produce((state, action) => {
-
-}, null);
-
-export default yourOnlineTag;
