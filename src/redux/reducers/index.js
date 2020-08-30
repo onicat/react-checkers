72204055ce7b1b06436e14cf168a70cf36c6eba7
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-30 03:07:17 +0300

    Connected onlineTag to store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,5 +3,6 @@ import { combineReducers } from 'redux'
 import settings from './settings'
 import board from './board'
 import currentPlayer from './currentPlayer'
+import onlineTag from './onlineTag'
 
-export default combineReducers({ currentPlayer, board, settings });
+export default combineReducers({ currentPlayer, onlineTag, board, settings });
