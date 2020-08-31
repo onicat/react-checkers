876c0f8932519369f29de6f1d43ec6155cc9ec0b
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-31 22:47:10 +0300

    Added MenuDivider to Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import 'styles/Menu.css'
 import Button from './Button'
+import MenuDivider from './MenuDivider';
 
 const Menu = () => {
   return (
     <div className='Menu'>
       <Button>Create room</Button>
+      <MenuDivider/>
       <Button>Join</Button>
+      <MenuDivider/>
       <Button>Exit</Button>
     </div>
   )
