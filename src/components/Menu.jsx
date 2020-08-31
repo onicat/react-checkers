0f47179a9eaaef37da59865bbde8c70409314aa1
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-31 23:22:45 +0300

    Added InputForm to Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 import 'styles/Menu.css'
 import Button from './Button'
 import MenuDivider from './MenuDivider';
+import InputForm from './InputForm';
 
 const Menu = () => {
   return (
     <div className='Menu'>
       <Button>Create room</Button>
       <MenuDivider/>
-      <Button>Join</Button>
+      <InputForm type='short' buttonText='Join'></InputForm>
       <MenuDivider/>
       <Button>Exit</Button>
     </div>
