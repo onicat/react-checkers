20e212aa74a995fbe8a9c5b806f021a8e58ca2fc
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-01 01:43:50 +0300

    Created changeStage action handler in stage reducer

--- src/redux/reducers/stage.js
+++ src/redux/reducers/stage.js
@@ -1,11 +1,22 @@
 import produce from 'immer'
 
 import { STAGES } from 'js/constants';
+import actionTypes from 'redux/actionTypes';
 
 const initialState = STAGES.OFFLINE;
 
 const stage = produce((state, action) => {
+  switch(action.type) {
+    case actionTypes.CHANGE_STAGE: {
 
+      switch(action.stage) {
+        case STAGES.OFFLINE: return STAGES.OFFLINE;
+        case STAGES.CONNECTING: return STAGES.CONNECTING;
+        case STAGES.WAITING_FOR_PLAYER: return STAGES.WAITING_FOR_PLAYER;
+      }
+
+    }
+  }
 }, initialState);
 
 export default stage;
