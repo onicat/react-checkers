7a86c1855342705c01644340f2eabc1c75daf0a5
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-01 16:14:03 +0300

    Extended changeOnlineTag action handler with null value

--- src/redux/reducers/onlineTag.js
+++ src/redux/reducers/onlineTag.js
@@ -5,9 +5,13 @@ import { PLAYERS_TAGS } from 'js/constants';
 const onlineTag = produce((state, action) => {
   switch(action.type) {
     case actionTypes.CHANGE_ONLINE_TAG: {
-      return (action.playerTag === PLAYERS_TAGS.PLAYER1) ?
-        PLAYERS_TAGS.PLAYER1:
-        PLAYERS_TAGS.PLAYER2
+      
+      switch(action.playerTag) {
+        case PLAYERS_TAGS.PLAYER1: return PLAYERS_TAGS.PLAYER1;
+        case PLAYERS_TAGS.PLAYER2: return PLAYERS_TAGS.PLAYER2;
+        case null: return null;
+      }
+
     }
   }
 }, null);
