650f6a4bd73bde27731cbb1a824899198851b111
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-03 00:35:40 +0300

    Revert "Created changeRoomId action"
    This reverts commit f62e4bf7aa325b07ae06600482b354a70ba5ca8c.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -32,8 +32,3 @@ export const resetBoard = () => ({
 export const resetCurrentPlayer = () => ({
   type: actionTypes.RESET_CURRENT_PLAYER
 });
-
-export const changeRoomId = (id) => ({
-  type: actionTypes.CHANGE_ROOM_ID,
-  id
-});
