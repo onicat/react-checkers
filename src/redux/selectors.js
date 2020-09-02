9192f996246adcaf6b721f852f8b7792a9b8dabe
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-02 23:54:36 +0300

    Created getRoomId selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,4 @@
 export const getBoard = store => store.board;
 export const getCurrentPlayer = store => store.currentPlayer;
 export const getStage = store => store.stage;
+export const getRoomId = store => store.roomId;
