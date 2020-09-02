da40bdc2c8f7e1efa10f0df0284173cfb9b9019f
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-03 00:34:58 +0300

    Revert "Created getOnlineTag selector"
    This reverts commit b68349e312613cf0a866641a139208a0824f0fbc.

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,4 +2,3 @@ export const getBoard = store => store.board;
 export const getCurrentPlayer = store => store.currentPlayer;
 export const getStage = store => store.stage;
 export const getRoomId = store => store.roomId;
-export const getOnlineTag = store => store.onlineTag;
