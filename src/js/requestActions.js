d0c46f273fe2069ae1f9c520de7cdd1dc703a55b
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-03 00:35:05 +0300

    Revert "Created sendChatMessage request action"
    This reverts commit fb170e0903107855fd6d9295ef4d12a2206de05e.

--- src/js/requestActions.js
+++ src/js/requestActions.js
@@ -12,13 +12,6 @@ class RequestActions {
       payload: {id}
     });
   }
-
-  sendChatMessage(roomId, senderTag) {
-    return JSON.stringify({
-      type: 'SEND_CHAT_MESSAGE',
-      payload: {roomId, senderTag}
-    });
-  }
 }
 
 export default new RequestActions();
