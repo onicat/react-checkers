d28f2aec1ba20bcc0e0c814168b2a158cb110516
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-03 00:35:29 +0300

    Revert "Created getRoomId selector"
    This reverts commit 9192f996246adcaf6b721f852f8b7792a9b8dabe.

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,3 @@
 export const getBoard = store => store.board;
 export const getCurrentPlayer = store => store.currentPlayer;
 export const getStage = store => store.stage;
-export const getRoomId = store => store.roomId;
