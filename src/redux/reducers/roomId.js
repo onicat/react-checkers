839c02438073a7bf386ec60bb49c42fd099b3855
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-03 00:35:37 +0300

    Revert "Created changeRoomId action handler in roomId reducer"
    This reverts commit 0f8ddfcfcc9100e5850f342d7f917f824885f8c2.

--- src/redux/reducers/roomId.js
+++ src/redux/reducers/roomId.js
@@ -1,14 +1,9 @@
 import produce from 'immer'
-import actionTypes from 'redux/actionTypes';
 
 const initialState = null;
 
 const roomId = produce((state, action) => {
-  switch(action.type) {
-    case actionTypes.CHANGE_ROOM_ID: {
-      return action.id;
-    }
-  }
+  
 }, initialState);
 
 export default roomId;
