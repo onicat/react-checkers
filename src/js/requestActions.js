c4d346f08577cda89fe45c8029b6c791c4acd3cc
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-03 19:16:46 +0300

    Added sendChatMessage request action

--- src/js/requestActions.js
+++ src/js/requestActions.js
@@ -12,6 +12,13 @@ class RequestActions {
       payload: {id, initiatorTag}
     });
   }
+
+  sendChatMessage(senderTag, text) {
+    return JSON.stringify({
+      type: 'SEND_CHAT_MESSAGE',
+      payload: {senderTag, text}
+    });
+  }
 }
 
 export default new RequestActions();
