ff1ae9524284ab9e3997619451e72b5e06751046
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-03 18:28:53 +0300

    Created getOnlineTag selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,4 @@
 export const getBoard = store => store.board;
 export const getCurrentPlayer = store => store.currentPlayer;
 export const getStage = store => store.stage;
+export const getOnlineTag = store => store.onlineTag;
