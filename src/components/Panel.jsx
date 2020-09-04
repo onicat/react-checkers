cebf4edca53e550de4f566aae3892a8f3e12c42c
Alexey Onikov <orbenaixasej@gmail.com>
2020-09-04 21:09:42 +0300

    Replaced playerColor variable with getPlayerColor function

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -4,19 +4,19 @@ import 'styles/Panel.css'
 import { PLAYERS_COLORS, PLAYERS_TAGS, STAGES } from 'js/constants'
 
 const Panel = ({currentPlayer, onlineTag, stage}) => {
-  const playerColor = (() => {
-    if (currentPlayer === PLAYERS_TAGS.PLAYER1) {
+  const getPlayerColor = (playerTag) => {
+    if (playerTag === PLAYERS_TAGS.PLAYER1) {
       return PLAYERS_COLORS.PLAYER1;
     } else {
       return PLAYERS_COLORS.PLAYER2;
     }
-  })();
+  };
   
   return (
     <div className='Panel'>
       <h2 className='Panel__item Panel__item_player'>
         Player:&nbsp;
-        <span style={{color: playerColor}}>
+        <span style={{color: getPlayerColor(currentPlayer)}}>
           {currentPlayer}
         </span>
       </h2>
