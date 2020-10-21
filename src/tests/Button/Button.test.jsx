aee8af366ab4e842f022901a85aaa69f65ad7f4b
Alexey Onikov <orbenaixasej@gmail.com>
2020-10-21 17:08:43 +0300

    Fixed code style

--- src/tests/Button/Button.test.jsx
+++ src/tests/Button/Button.test.jsx
@@ -6,5 +6,5 @@ import toMatchSnapshot from 'tests/toMatchSnapshot';
 describe('<Button />', () => {
   it('Render Button without className', () => {
     toMatchSnapshot(<Button>Button</Button>);
-  })
+  });
 });
